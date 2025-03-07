a060d0d0f069c93f30e29ded6aad3e5504f39986
Tim Beinert <tim.beinert@t-online.de>
2025-03-07 18:22:01 +0100

    AdalineGD Model

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+""" Python Machine Learning - S.Raschka / V.Mirjalili """
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from perceptron import Perceptron
+from adalineGD import AdalineGD
 
 s = os.path.join('https://archive.ics.uci.edu', 'ml',
                  'machine-learning-databases',
@@ -40,7 +42,7 @@ plt.plot(range(1, len(ppn.errors_) + 1),
          ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
-plt.show()
+#plt.show()
 
 """ Convenience function to plot decision boundary """
 def plot_decision_regions(X, y, classifier, resolution=0.02):
@@ -74,4 +76,26 @@ plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
+#plt.show()
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=10, eta=0.01)
+ada1.fit(X, y)
+
+ax[0].plot(range(1, len(ada1.cost_) + 1),
+           np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001)
+ada2.fit(X, y)
+
+ax[1].plot(range(1, len(ada2.cost_) + 1),
+           ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
 plt.show()
